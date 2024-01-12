1832e69cefc05684bc0740931deb015aa4c6eb92
alvaro11lar <alvaro200711@gmail.com>
2024-01-12 10:03:55 +0100

    aaa

--- src/application/Main.java
+++ src/application/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			//a
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
